7e146b0a00ce951f476fc26622483101ea371bc0
AlexeyPrutik <alexeyprutik@gmail.com>
2016-10-22 21:51:42 +0300

    Lex-FirstCommit

---
+++ LearningAll/Terms/DelegateLearning.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningAll.Terms
+{
+    class MyDelegate
+    {
+        public delegate void Del(string message);
+        public void MethodWithCallback(int param1, int param2, Del callback)
+        {
+            callback("The number is: " + (param1 + param2).ToString());
+        }
+    }
+}
